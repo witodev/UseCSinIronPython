c047174ad3bf4bfc1f05bb02d0db171cb6c81b45
witodev <szwed.witold@gmail.com>
2017-07-24 18:47:58 +0200

    More data

--- LibClient/LibClient.py
+++ LibClient/LibClient.py
@@ -1,20 +1,26 @@
 import System.Collections.Generic
+import random
+
 import clr    
 clr.AddReferenceToFileAndPath(r"c:\Users\wszwe\Documents\Visual Studio 2017\Projects\UseCSinIronPython\Library\bin\Debug\Library.dll")
 import Library
 
 print('Hello world')
 
-data = Library.USUM()
-data.id = 1
-data.x = 123.123
-data.y = 432.112
-data.z = 342.645
-
 d = System.Collections.Generic.Dictionary[int, Library.USUM]()
-d.Add(data.id, data)
+for i in range(10):
+    data = Library.USUM()
+    data.id = i+1 
+    data.x = random.randint(0,100)/10.0
+    data.y = random.randint(0,100)/10.0
+    data.z = random.randint(0,100)/10.0
+    d.Add(data.id, data)
 
 tmp = Library.FEM.SaveToFile(d, "test.txt");
 
 print("Siema")
+
+for i in tmp:
+    print(i.Key)
+    print(i.Value)
 
