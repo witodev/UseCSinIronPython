4fac173f8382ba10c1b4b30a297bfb7d86e5e8af
witodev <szwed.witold@gmail.com>
2017-07-24 20:38:58 +0200

    Clearing

--- LibClient/LibClient.py
+++ LibClient/LibClient.py
@@ -5,22 +5,17 @@ import clr
 clr.AddReferenceToFileAndPath(r"c:\Users\wszwe\Documents\Visual Studio 2017\Projects\UseCSinIronPython\Library\bin\Debug\Library.dll")
 import Library
 
-print('Hello world')
-
-d = System.Collections.Generic.Dictionary[int, Library.USUM]()
+collector = System.Collections.Generic.Dictionary[int, Library.USUM]()
 for i in range(10):
     data = Library.USUM()
     data.id = i+1 
     data.x = random.randint(0,100)/10.0
     data.y = random.randint(0,100)/10.0
     data.z = random.randint(0,100)/10.0
-    d.Add(data.id, data)
+    collector.Add(data.id, data)
 
-tmp = Library.FEM.SaveToFile(d, "test.txt");
-
-print("Siema")
+tmp = Library.FEM.SaveToFile(collector, "test.txt");
 
 for i in tmp:
-    print(i.Key)
-    print(i.Value)
+    print(i.Key, i.Value.ToString())
 
